5ad0c0c289ab7de46dc89cf0376ec74242af8a6d
jeisjaojw <110499855+jeisjaojw@users.noreply.github.com>
2022-08-05 16:45:45 +0800

    Update xeon.js

--- xeon.js
+++ xeon.js
@@ -182,12 +182,127 @@ XeonLft = await getBuffer(`https://hardianto.xyz/api/goodbye3?profile=${encodeUR
    │✑ ${xmembers}th
    │✑  𝗝𝗼𝗶𝗻𝗲𝗱 : 
    │✑ ${xtime} ${xdate}
+   │✑ New mem intro
+Yang baru masuk intro dulu gan
+
+Nama lengkap: 
+Nama panggilan: 
+Hobi:
+Umur: 
+Gender: 
+Kelas: 
+Tinggi badan: 
+Berat badan : 
+Agama: 
+Golongan darah:
+Status: 
+Nama pacar:
+Jumlah mantan: 
+Nama mantan: 
+Nama bapak : 
+Nama ibu :
+Nama kakak:
+Kakak online:
+Kakak kandung/tiri:
+Jumlah kakak: 
+Nama adek:
+Adek online:
+Adek kandung/tiri:
+Jumlah adek: 
+Nama kakek:
+Kakek dari ayah :
+Kakek dari ibu :
+Nama nenek
+Nenek dari ayah :
+Nenek dari ibu :
+Nama bibi
+Bibi dari ayah :
+Bibi dari ibu :
+Nama paman
+Bibi dari ayah :
+Bibi dari ibu :
+KTP: 
+SIM: 
+STNK: 
+BPKB:
+KK: 
+Alamat rumah:
+RT: 
+RW:
+KELURAHAN: 
+KECAMATAN: 
+KABUPATEN: 
+KOTA: 
+PROVINSI:
+PLANET:
+GALAXY:
+UNIVERSE:
+LANGIT:
+DARATAN: 
+LAUTAN: 
+KEPULAUAN:
+SAMUDRA: 
+UKURAN SEPATU:
+UKURAN BAJU: 
+UKURAN CELANA: 
+LEBAR PINGGANG: 
+PANJANG TANGAN:
+PANJANG KAKI:
+MAKANAN FAVORIT:
+MINUMAN FAVORIT:
+FILM FAVORIT:
+SINETRON FAVORIT:
+GAME FAVORIT:
+ANIME FAVORIT:
+MANGA FAVORIT:
+MANHUA FAVORIT:
+MANHWA FAVORIT:
+CHANNEL YOUTUBE:
+INSTAGRAM:
+TWITTER: 
+FACEBOOK:
+MUSIC FAVORIT:
+SIFAT:
+SIKAP: 
+ZODIAK:
+TANGGAL LAHIR:
+MERK HP:
+MERK MOTOR:
+MERK MOBIL:
+TINGKAT RUMAH:
+ALAMAT SEKOLAH:
+Ukuran daleman:
+Ukuran atasan : 
+Diameter kepala :
+
+Statistik tubuh 
+Diameter perut :
+Diameter lengan :
+Diameter paha :
+Diameter lutut :
+Diameter betis:
+Panjang tangan :
+Panjang kaki :
+Panjang kepala :
+Lebar hidung :
+Cita cita :
+Hobi :
+Jenis hewan peliharaan :
+Nama hewan:
+Diameter rumah:
+Waifu:
+Husbu:
+Loli kesukaan :
+Shota kesukaan :
+Punya brp teman :
+Teman online :
+Tema
    └───────────────┈ ⳹`
       //if you copy the code value,
    //dont forget to put my name(Xeon) as credit
    //you fail to put, i sue you for sure!
 let buttons = [
-{buttonId: `wkwwk`, buttonText: {displayText: 'Welcome 💐'}, type: 1}
+{buttonId: `wkwwk`, buttonText: {displayText: 'Selamat Datang'}, type: 1}
 ]
 let buttonMessage = {
 document: fs.readFileSync('./XeonMedia/theme/cheems.xlsx'),
@@ -231,7 +346,7 @@ XeonBotInc.sendMessage(anu.id, buttonMessage, {quoted:unicorndoc})
    //dont forget to put my name(Xeon) as credit
    //you fail to put, i sue you for sure!
 let buttons = [
-{buttonId: `wkwkwk`, buttonText: {displayText: 'Sayonara 🥀'}, type: 1}
+{buttonId: `wkwkwk`, buttonText: {displayText: 'Sayonara,Jangan Balik Lagi Kontol'}, type: 1}
 ]
 let buttonMessage = {
 document: fs.readFileSync('./XeonMedia/theme/cheems.xlsx'),
