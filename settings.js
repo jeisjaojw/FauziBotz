3634a54ca660c86cf3ee3cb30cf970034ce6cb6c
jeisjaojw <110499855+jeisjaojw@users.noreply.github.com>
2022-08-18 22:00:01 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,20 +54,20 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['+19146271508'] //ur owner number
+global.vcardowner = ['19146271508'] //ur owner number
 global.ownername = "FauziGanteng" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['+19146271508']
-global.ownertag = '+19146271508' //ur tag number
+global.owner = ['19146271508']
+global.ownertag = '19146271508' //ur tag number
 global.botname = 'FauziBotz-Md' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "💦" //ur menu react emoji
+global.reactmoji = "🚀" //ur menu react emoji
 global.themeemoji = "💀" //ur theme emoji
 global.packname = "Dibuat Oleh" //ur sticker watermark packname
 global.author = "Fauzi" //ur sticker watermark author
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['+19146271508'] //ur premium numbers
+global.premium = ['19146271508'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
